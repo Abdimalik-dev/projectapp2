82ff0339273c4493754618df036f0cb4cf24ef20
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-12-06 20:45:16 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -16,3 +16,59 @@ export default function App() {
 }
 
 export default App;
+
+import React, { useState } from 'react'
+import { Todolist } from './Todolist'
+
+export default function App() {
+  const [todos, setTodos] = useState(['Todo 1 '. 'Todo 2'])
+  return ( 
+  <>
+    <TodoList todoList={todos} />
+   <input type='text'/>
+   <button>Add TOdo</button>
+   <button> clear completed todos</button>
+    <div> 0 left to do </div>
+  </>
+
+  )
+}
+
+export default App;
+
+
+import React, { useState } from 'react'
+import { Todolist } from './Todolist'
+
+export default function App() {
+  const [todos, setTodos] = useState(['Todo 1 '. 'Todo 2'])
+  return ( 
+  <>
+    <TodoList todoList={todos} />
+   <input type='text'/>
+   <button>Add TOdo</button>
+   <button> clear completed todos</button>
+    <div> 0 left to do </div>
+  </>
+
+  )
+}
+
+export default App;import React, { useState } from 'react'
+import { Todolist } from './Todolist'
+
+export default function App() {
+  const [todos, setTodos] = useState(['Todo 1 '. 'Todo 2'])
+  return ( 
+  <>
+    <TodoList todoList={todos} />
+   <input type='text'/>
+   <button>Add TOdo</button>
+   <button> clear completed todos</button>
+    <div> 0 left to do </div>
+  </>
+
+  )
+}
+
+export default App;
